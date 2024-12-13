3d7337aa2d636c14cbb701cadddbc9bb0d5c23c8
Manoj <Manojkumar8179178043@gmail.com>
2024-12-13 09:31:47 +0530

    Added React project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Pushpa 2: The Rule</h1>
+      <h3>Director: Sukumar</h3>
+      <img src="./AAimg/Pushpa.png" alt="Pushpa" />
+    </div>
+  );
+}
+
+export default App;
