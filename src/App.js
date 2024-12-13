05ad99b98f766b603b403a05096e1865dfcfd3f6
Manoj <106075652+manoj8043@users.noreply.github.com>
2024-12-13 09:40:23 +0530

    Added 2nd headding

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <h1>Pushpa 2: The Rule</h1>
       <h3>Director: Sukumar</h3>
       <img src="./AAimg/Pushpa.png" alt="Pushpa" />
+      <h1>2nd Title</h1>
     </div>
   );
 }
